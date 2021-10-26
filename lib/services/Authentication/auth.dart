0cfd33da20cb0307d1b2b42dcecb93210596cda9
MehmetKaranlik <mhmtkrnlkk@gmail.com>
2021-10-26 11:01:42 +0300

    Unnecessary removal

--- lib/services/Authentication/auth.dart
+++ lib/services/Authentication/auth.dart
@@ -1,6 +1,5 @@
 import 'package:chat_app_example/models/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 class AuthenticationMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
